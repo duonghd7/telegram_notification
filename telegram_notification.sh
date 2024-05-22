eca6a6f96a117b8f6a69dd74017c7b9b14b3825c
Trist <duonghd1307@gmail.com>
2024-05-22 12:43:51 +0700

    refactor: topic id

--- telegram_notification.sh
+++ telegram_notification.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 TELEGRAM_CHAT_ID=$1
-TELEGRAM_MESSAGE_TOPIC_ID=$2
+TELEGRAM_TOPIC_ID=$2
 TELEGRAM_MESSAGE=$3
 TELEGRAM_BOT_TOKEN=$4
 curl -X POST \
      -H "Content-Type: application/json" \
-     -d "{\"chat_id\": ${TELEGRAM_CHAT_ID}, \"message_thread_id\": \"${TELEGRAM_MESSAGE_TOPIC_ID}\", \"text\": \"${TELEGRAM_MESSAGE}\", \"disable_notification\": false}" \
+     -d "{\"chat_id\": ${TELEGRAM_CHAT_ID}, \"message_thread_id\": \"${TELEGRAM_TOPIC_ID}\", \"text\": \"${TELEGRAM_MESSAGE}\", \"disable_notification\": false}" \
      https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/sendMessage
